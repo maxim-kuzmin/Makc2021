ee8113dd9b2ae0c67a5cb76c4139ab6d39d3315c
maxim-kuzmin <maxim.kuzmin@inbox.ru>
2021-03-10 01:27:51 +0300

    -

---
+++ server/src/Makc2021.Layer3.Sample.ORMs.EF.DBs.SqlServer/EFSqlServerContext.cs
@@ -0,0 +1,53 @@
+﻿//Author Maxim Kuzmin//makc//
+
+using Makc2021.Layer3.Sample.ORMs.EF.Db;
+using Makc2021.Layer3.Sample.ORMs.EF.DBs.SqlServer.Db;
+
+namespace Makc2021.Layer3.Sample.ORMs.EF.DBs.SqlServer
+{
+    public class EFSqlServerContext
+    {
+        #region Properties
+
+        /// <summary>
+        /// Конфигурация.
+        /// </summary>
+        public EFSqlServerConfig Config { get; private set; }
+
+        /// <summary>
+        /// Фабрика базы данных.
+        /// </summary>
+        public EFDbFactory DbFactory { get; private set; }
+
+        /// <summary>
+        /// Настройки.
+        /// </summary>
+        public Settings Settings { get; } = EFSqlServerSettings.Instance;
+
+        #endregion Properties
+
+        #region Constructor
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="config">Конфигурация.</param>
+        /// <param name="externals">Внешнее.</param>
+        public EFSqlServerContext(
+            EFSqlServerConfig
+            config,
+            EFSqlServerExternals externals
+            )
+        {
+            Config = config;
+
+            DbFactory = new EFSqlServerDbFactory(
+                Config.Settings.ConnectionString,
+                Settings,
+                externals.Environment
+                );
+        }
+
+        #endregion Constructo
+    }
+}
---
+++ server/src/Makc2021.Layer3.Sample.ORMs.EF.DBs.SqlServer/EFSqlServerModule.cs
@@ -0,0 +1,62 @@
+﻿//Author Maxim Kuzmin//makc//
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Makc2021.Layer3.Sample.ORMs.EF.DBs.SqlServer
+{
+    using Makc2021.Layer1;
+
+    /// <summary>
+    /// ORM "Entity Framework". База данных "Microsoft SQL Server". Модуль.
+    /// </summary>
+    public class EFSqlServerModule
+    {
+        #region Properties
+
+        /// <summary>
+        /// Конфигурация.
+        /// </summary>
+        public EFSqlServerConfig Config { get; private set; }
+
+        /// <summary>
+        /// Контекст.
+        /// </summary>
+        public EFSqlServerContext Context { get; private set; }
+
+        #endregion Properties
+
+        #region Public methods
+
+        /// <summary>
+        /// Настроить сервисы.
+        /// </summary>
+        /// <param name="services">Сервисы.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(x => Config);
+            services.AddTransient(x => Config.Settings);
+            services.AddTransient(x => Context.DbFactory);
+            services.AddTransient(x => Context.Settings);
+        }
+
+        /// <summary>
+        /// Инициализировать конфигурацию.
+        /// </summary>
+        /// <param name="environment">Окружение.</param>
+        public void InitConfig(Environment environment)
+        {
+            Config = new EFSqlServerConfig(environment);
+        }
+
+        /// <summary>
+        /// Инициализировать контекст.
+        /// </summary>
+        /// <param name="externals">Внешнее.</param>
+        public void InitContext(EFSqlServerExternals externals)
+        {
+            Context = new EFSqlServerContext(Config, externals);
+        }
+
+        #endregion Public methods
+    }
+}
