fd4876667727c1eab72e097576613daa98542187
maxim-kuzmin <maxim.kuzmin@inbox.ru>
2021-07-05 19:39:04 +0300

    -

--- client/makc2021-react-redux-ts/src/Layer6/Controls/ControlsContext.ts
+++ client/makc2021-react-redux-ts/src/Layer6/Controls/ControlsContext.ts
@@ -2,7 +2,6 @@
 
 import { Context as Layer1Context } from 'src/Layer1/Context';
 import { MenuControlsContext } from './Menus/MenuControlsContext';
-import { MessageControlsContext } from './Messages/MessageControlsContext';
 import { NotificationControlsContext } from './Notifications/NotificationControlsContext';
 import { TableControlsContext } from './Tables/TableControlsContext';
 
@@ -14,11 +13,6 @@ export class ControlsContext {
    * Меню.
    */
   readonly Menus = new MenuControlsContext();
-
-  /**
-   * Сообщения.
-   */
-  readonly Messages = new MessageControlsContext();
 
   /**
    * Извещения.
@@ -36,7 +30,6 @@ export class ControlsContext {
    */
   configureServices(contextOfLayer1: Layer1Context) {
     this.Menus.configureServices(contextOfLayer1);
-    this.Messages.configureServices(contextOfLayer1);
     this.Notification.configureServices(contextOfLayer1);
     this.Tables.configureServices(contextOfLayer1);
   }
